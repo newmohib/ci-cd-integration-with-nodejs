f3c973278dc6bd26f2574406ba11d627c3ede427
Mohibur Rahman <mohib@bs-23.net>
2023-03-04 13:00:58 +0600

    update api

--- app.js
+++ app.js
@@ -22,6 +22,16 @@ app.post('/', (req, res) => {
     //cluster.Worker.kill(); 
 });
 
+app.get('/authe/signin', (req, res) => {
+    console.log(req.body, new Date());
+    for (let i = 0; i < 1e8; i++) { 
+
+    }
+    console.log(new Date());
+    res.send({code: 200, pid: process.pid})
+    //cluster.Worker.kill(); 
+});
+
 app.get('/cpu', (req, res) => {
     res.send({code: 200, numOfCpu: numCpu, isMaster: cluster.isMaster})
 });
